96eca9b840bc5913ba9153a4d0620ae82f4b3221
arcticmatt <mlimcollege@gmail.com>
2014-11-02 15:57:07 -0800

    change name of lectures, make one large pdf for pre-midterm material

--- psets/pset4/cs121hw4/make-branch-stats.sql
+++ psets/pset4/cs121hw4/make-branch-stats.sql
@@ -1,27 +1,147 @@
 -- [Problem 1]
-
+-- Create index on branch_name because branch_name is used in a bunch of WHERE
+-- statements
+CREATE INDEX idx_branch_name ON account(branch_name);
 
 
 -- [Problem 2]
-
+-- This table mimics a materialized view, and contains statistics for
+-- branches.
+CREATE TABLE mv_branch_account_stats (
+    branch_name    VARCHAR(15),
+    num_accounts   INTEGER NOT NULL DEFAULT 0,
+    total_deposits INTEGER NOT NULL DEFAULT 0,
+    min_balance    NUMERIC(12, 2) NOT NULL DEFAULT 0,
+    max_balance    NUMERIC(12, 2) NOT NULL DEFAULT 0,
+    PRIMARY KEY (branch_name)
+);
 
 
 -- [Problem 3]
-
+-- Populate the table with values from the account table.
+INSERT INTO mv_branch_account_stats
+    SELECT branch_name,
+        COUNT(*) AS num_accounts,
+        SUM(balance) AS total_deposits,
+        MIN(balance) AS min_balance,
+        MAX(balance) AS max_balance
+    FROM account GROUP BY branch_name;
 
 
 -- [Problem 4]
-
+-- Create a view on mv_branch_account_stats. This view includes avg_balance,
+-- which is calculated as a derived attribute for efficiency.
+CREATE VIEW branch_account_stats AS
+    SELECT branch_name,
+        num_accounts,
+        total_deposits,
+        (total_deposits / num_accounts) AS avg_balance,
+        min_balance,
+        max_balance
+    FROM mv_branch_account_stats GROUP BY branch_name;
 
 
 -- [Problem 5]
+DELIMITER !
 
+-- Stored procedure for updating mv_branch_account_stats on an insert into
+-- account. Used in trg_insert and trg_update.
+CREATE PROCEDURE sp_insert(
+    IN sp_branch_name VARCHAR(15),
+    IN sp_balance NUMERIC(12, 2)
+)
+BEGIN
+    IF sp_branch_name IN (SELECT branch_name FROM mv_branch_account_stats)
+    THEN
+        -- If not the first account for a particular branch, update the branch
+        UPDATE mv_branch_account_stats
+            SET num_accounts = num_accounts + 1,
+                total_deposits = total_deposits + sp_balance,
+                min_balance = LEAST(min_balance, sp_balance),
+                max_balance = GREATEST(max_balance, sp_balance)
+            WHERE branch_name = sp_branch_name;
+    ELSE
+        -- Else, insert the sumary info for the new branch
+        INSERT INTO mv_branch_account_stats
+            VALUES (sp_branch_name, 1, sp_balance, sp_balance, sp_balance);
+    END IF;
+END!
+
+DELIMITER ;
+
+DELIMITER !
+
+-- Insert trigger on account that updates mv_branch_account_stats accordingly.
+CREATE TRIGGER trg_insert AFTER INSERT ON account FOR EACH ROW
+BEGIN
+    CALL sp_insert(NEW.branch_name, NEW.balance);
+END!
+
+DELIMITER ;
 
 
 -- [Problem 6]
+DELIMITER !
 
+-- Stored procedure for updating mv_branch_account_stats on a delete from
+-- account. Used in trg_delete and trg_update.
+CREATE PROCEDURE sp_delete(
+    IN sp_branch_name VARCHAR(15),
+    IN sp_balance NUMERIC(12, 2)
+)
+BEGIN
+    IF 1 IN (SELECT num_accounts FROM mv_branch_account_stats WHERE
+    branch_name = sp_branch_name) THEN
+        -- If the deleted account is the last account for the branch, delete
+        -- the branch from mv_branch_account_stats.
+        DELETE FROM mv_branch_account_stats WHERE branch_name = sp_branch_name;
+    ELSE
+        -- Else, update the branch.
+        UPDATE mv_branch_account_stats
+            SET num_accounts = num_accounts - 1,
+                total_deposits = total_deposits - sp_balance,
+                min_balance = (SELECT MIN(balance) FROM account WHERE
+                    branch_name = sp_branch_name),
+                max_balance = (SELECT MAX(balance) FROM account WHERE
+                    branch_name = sp_branch_name)
+            WHERE branch_name = sp_branch_name;
+    END IF;
+END!
+
+DELIMITER ;
+
+DELIMITER !
+
+-- Delete trigger on account that updates mv_branch_account_stats accordingly.
+CREATE TRIGGER trg_delete AFTER DELETE ON account FOR EACH ROW
+BEGIN
+    CALL sp_delete(OLD.branch_name, OLD.balance);
+END!
+
+DELIMITER ;
 
 
 -- [Problem 7]
+DELIMITER !
 
+-- Update trigger on account that updates mv_branch_account_stats accordingly.
+CREATE TRIGGER trg_update AFTER UPDATE ON account FOR EACH ROW
+BEGIN
+    IF OLD.branch_name = NEW.branch_name THEN
+        -- If the branch name did not change, update the summary info
+        UPDATE mv_branch_account_stats
+            SET total_deposits = total_deposits - OLD.balance + NEW.balance,
+                min_balance = (SELECT MIN(balance) FROM account WHERE
+                    branch_name = OLD.branch_name),
+                max_balance = (SELECT MAX(balance) FROM account WHERE
+                    branch_name = OLD.branch_name)
+            WHERE branch_name = OLD.branch_name;
+    ELSE
+        -- Else, delete the old account from the old branch and add the new
+        -- account to the new branch
+        CALL sp_delete(OLD.branch_name, OLD.balance);
+        CALL sp_insert(NEW.branch_name, NEW.balance);
+    END IF;
+END!
 
+DELIMITER ;
--- psets/pset4/cs121hw4/submit-stats.sql
+++ psets/pset4/cs121hw4/submit-stats.sql
@@ -1,15 +1,139 @@
 -- [Problem 1]
+DELIMITER !
 
+DROP FUNCTION IF EXISTS min_submit_interval;
+CREATE FUNCTION min_submit_interval(passed_id INTEGER) RETURNS INTEGER
+BEGIN
+    -- Variables to store values into
+    DECLARE first_val INTEGER;  -- store current date value here
+    DECLARE second_val INTEGER; -- store next date value here
+    DECLARE diff INTEGER;
+    DECLARE min_diff INTEGER DEFAULT NULL;
 
+    -- Cursor, and flag for when fetching is done.
+    DECLARE done INT DEFAULT 0;
+    DECLARE cur CURSOR FOR
+        SELECT UNIX_TIMESTAMP(sub_date) AS sub_date
+        FROM fileset
+        WHERE sub_id = passed_id ORDER BY sub_date;
+    -- When fetch is complete, handler sets flag.
+    -- 0200 is MySql error for "zero rows fetched."
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000'
+        SET done = 1;
+
+    -- Open cur, loop through cur to find min_diff, close cur
+    OPEN cur;
+    FETCH cur INTO first_val;
+    WHILE NOT done DO
+        FETCH cur INTO second_val;
+        -- Avoid using a null second_val
+        IF NOT done THEN
+            SET diff = second_val - first_val;
+            -- The default value for min_diff is NULL, so take care of that
+            IF diff < IFNULL(min_diff, 2147483647) THEN SET min_diff = diff;
+            END IF;
+            SET first_val = second_val;
+        END IF;
+    END WHILE;
+    CLOSE cur;
+    RETURN min_diff;
+END!
+
+DELIMITER ;
+
+-- TEST QUERIES
+-- select UNIX_TIMESTAMP(sub_date) from fileset where sub_id = 3798;
+-- select sub_id, min_submit_interval(sub_id) from fileset order by sub_id;
 
 -- [Problem 2]
+DELIMITER !
 
+DROP FUNCTION IF EXISTS max_submit_interval;
+CREATE FUNCTION max_submit_interval(passed_id INTEGER) RETURNS INTEGER
+BEGIN
+    -- Variables to store values into
+    DECLARE first_val INTEGER;
+    DECLARE second_val INTEGER;
+    DECLARE diff INTEGER;
+    DECLARE max_diff INTEGER DEFAULT NULL;
 
+    -- Cursor, and flag for when fetching is done
+    DECLARE done INT DEFAULT 0;
+    DECLARE cur CURSOR FOR
+        SELECT UNIX_TIMESTAMP(sub_date) AS sub_date
+        FROM fileset
+        WHERE sub_id = passed_id ORDER BY sub_date;
+    -- When fetch is ocmplete, handler sets flag.
+    -- 0200 is MySql error for "zero rows fetched."
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000'
+        SET done = 1;
+
+    -- Open cur, loop through cur to find max_diff, close cur
+    OPEN cur;
+    FETCH cur INTO first_val;
+    WHILE NOT done DO
+        FETCH cur INTO second_val;
+        -- Avoid using a null second_val
+        IF NOT done THEN
+            SET diff = second_val - first_val;
+            -- The default value for max_diff is NULL, so take care of that
+            IF diff > IFNULL(max_diff, 0) THEN SET max_diff = diff;
+            END IF;
+            SET first_val = second_val;
+        END IF;
+    END WHILE;
+    CLOSE cur;
+    RETURN max_diff;
+END!
+
+DELIMITER ;
+
+-- TEST QUERY
+-- select sub_id, max_submit_interval(sub_id) from fileset order by sub_id;
 
 -- [Problem 3]
+DELIMITER !
 
+DROP FUNCTION IF EXISTS avg_submit_interval;
+CREATE FUNCTION avg_submit_interval(passed_id INTEGER) RETURNS DOUBLE BEGIN
+    DECLARE max_date INTEGER;
+    DECLARE min_date INTEGER;
+    -- The number of dates (one more than the number of intervals)
+    DECLARE count_dates INTEGER;
+    DECLARE avg_interval DOUBLE;
 
+    SELECT UNIX_TIMESTAMP(MAX(sub_date)) INTO max_date
+    FROM fileset
+    WHERE sub_id = passed_id;
+
+    SELECT UNIX_TIMESTAMP(MIN(sub_date)) INTO min_date
+    FROM fileset
+    WHERE sub_id = passed_id;
+
+    SELECT count(sub_date) INTO count_dates
+    FROM fileset
+    WHERE sub_id = passed_id;
+
+    -- The average interval is just the max_date minus the min_date divided
+    -- by the number of intervals.
+    SET avg_interval = (max_date - min_date) / (count_dates - 1);
+
+    RETURN avg_interval;
+END!
+
+DELIMITER ;
+
+-- TEST QUERIES
+-- select sub_id, avg_submit_interval(sub_id) from fileset order by sub_id;
+-- SELECT sub_id, min_submit_interval(sub_id) AS min_interval,
+    -- max_submit_interval(sub_id) AS max_interval,
+    -- avg_submit_interval(sub_id) AS avg_interval
+-- FROM (SELECT sub_id FROM fileset
+    -- GROUP BY sub_id HAVING COUNT(*) > 1) AS multi_subs
+-- ORDER BY min_interval, max_interval LIMIT 20;
 
 -- [Problem 4]
-
-
+-- Create an index on sub_id because we call WHERE on sub_id often; thus,
+-- indexing this makes it much faster to search for sub_id and thus makes these
+-- WHEREs much faster
+CREATE INDEX idx_id ON fileset(sub_id);
